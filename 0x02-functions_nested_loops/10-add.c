80eb4c9fb0f600174a5f00f9641845deb3a58dcf
MyPenServices <nyaberiigeorge@gmail.com>
2023-08-16 09:11:09 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - function that sums two numbers
+ * @a: parameter one
+ * @b: parameter two
+ * Return: a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
